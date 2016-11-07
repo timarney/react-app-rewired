8178c87941721b7f213fd09cd84fd8c474ccf92f
Tim Arney <tim@line37.com>
2016-11-07 07:59:11 -0500

    preact test setup

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,19 @@
 module.exports = function override (config) {
+  
+  
+  config.resolve = {
+    "alias": {
+      "react": "preact-compat",
+      "react-dom": "preact-compat"
+    }
+  }
+  
+
   config.postcss = function () {
     var autoprefixer = require('autoprefixer')
     var eh = require('postcss-canadian-stylesheets')
     return [autoprefixer, eh]
   }
+  
   return config
 }
--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div class="App">
+        <div class="App-header">
+          <img src={logo} class="App-logo" alt="logo" />
           <h2>Welcome to Canada</h2>
         </div>
-        <p className="App-intro">
+        <p class="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
