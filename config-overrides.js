ac97163365494528f2fb0224936c437a5ed67cab
Tim Arney <timarney@users.noreply.github.com>
2016-11-14 14:15:48 -0500

    Tweaked default config-overrides.js file

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,15 @@
-/* config-overrides.js */
+/* CSS Modules Loader */
 module.exports = function override(config, env) {
-  var loader = '';
+  var loader = 'style!css?modules&';
   if (env === 'development') {
-    loader = 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss';
+    loader += 'importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss';
   } else {
-    loader = 'style!css?modules&-autoprefixer&importLoaders=1!postcss';
+    loader += '-autoprefixer&importLoaders=1!postcss';
   }
   // Find the right loader then patch its 'loader' property
   config.module.loaders.forEach(l => {
     if (String(l.test) == String(/\.css$/)) l.loader = loader
   })
-  // default override
+ 
   return config;
 }
