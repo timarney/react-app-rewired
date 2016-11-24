db6e4e20917c5255b15272f90e1977333ae4f04e
Tim Arney <tim@line37.com>
2016-11-24 14:04:38 -0500

    added bin dir to run scripts

---
+++ bin/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var spawn = require('cross-spawn');
+var script = process.argv[2];
+var args = process.argv.slice(3);
+
+switch (script) {
+case 'build':
+case 'start':
+  var result = spawn.sync(
+    'node',
+    [require.resolve('../scripts/' + script)].concat(args),
+    {stdio: 'inherit'}
+  );
+  process.exit(result.status);
+  break;
+default:
+  console.log('Unknown script "' + script + '".');
+  break;
+}
