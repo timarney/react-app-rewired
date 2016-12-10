69030feedd94fbf78bce4dc862969831a505f4ba
Pedro Antonio Souza Viegas <pedroasv@gmail.com>
2016-12-10 06:13:52 -0200

    basic cleaning

--- scripts/start.js
+++ scripts/start.js
@@ -1,11 +1,10 @@
 /* start.js */
-var fs = require('fs');
-var path = require('path');
-
-var config = require('react-scripts/config/webpack.config.dev')
-var override = require(path.resolve(fs.realpathSync(process.cwd()) + '/config-overrides'));
+const fs = require('fs');
+const path = require('path');
+const config = require('react-scripts/config/webpack.config.dev');
+const override = require(path.resolve(fs.realpathSync(process.cwd()) + '/config-overrides'));
 
 require.cache[require.resolve('react-scripts/config/webpack.config.dev')].exports =
-override(config, process.env.NODE_ENV || 'development');
+  override(config, process.env.NODE_ENV || 'development');
 
-require('react-scripts/scripts/start')
+require('react-scripts/scripts/start');
