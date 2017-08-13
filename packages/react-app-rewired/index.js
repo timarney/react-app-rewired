11bc0989d6b34f2342bc1e522ff53f11f802e0aa
Tim Arney <tim@line37.com>
2017-08-13 15:25:46 -0400

    update for react-script 1.0.11

--- packages/react-app-rewired/index.js
+++ packages/react-app-rewired/index.js
@@ -1,11 +1,41 @@
-const babelLoader = function(conf) {
+const checkRuleLegacy = function(conf) {
   if (!conf.loader) return false;
   return conf.loader.indexOf("babel-loader") > -1;
 };
 
+const getLoader = function(config) {
+  let babelLoader = false;
+  let isBabel = false;
+  const checkRule = rule =>
+    rule.loader && rule.loader.indexOf("babel-loader") != -1;
+
+  config.module.rules.forEach(rule => {
+    if (!rule.oneOf) {
+      isBabel = config.module.rules.find(checkRuleLegacy);
+
+      if (isBabel) {
+        babelLoader = isBabel;
+      }
+
+      return;
+    }
+
+    isBabel = rule.oneOf.find(checkRule);
+    if (isBabel) {
+      babelLoader = isBabel;
+    }
+  });
+
+  return babelLoader;
+};
+
 const injectBabelPlugin = function(pluginName, config) {
-  const babelrc = config.module.rules.find(babelLoader).options;
-  babelrc.plugins = [pluginName].concat(babelrc.plugins || []);
+  const loader = getLoader(config);
+  if (!loader) {
+    console.log("babel-loader not found");
+    return config;
+  }
+  loader.options.plugins = [pluginName].concat(loader.options.plugins || []);
   return config;
 };
 
