d7e7397206937eb5936b0d76b22b751bbff5df4f
Andreas Cederström <andriijas@users.noreply.github.com>
2017-09-25 12:33:41 +0200

    Load .env files via react-script to preserve correct priority order

--- packages/react-app-rewired/scripts/build.js
+++ packages/react-app-rewired/scripts/build.js
@@ -1,11 +1,8 @@
 /* build.js */
 process.env.NODE_ENV = 'production';
 
-// Load environment variables from .env file. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
+// Load environment variables from .env files
+require("react-scripts/config/env");
 
 const paths = require('./utils/paths');
 const webpackConfig = paths.scriptVersion + '/config/webpack.config.prod';
