070eeca1628691df5139438c75a5307b75db3451
Lee <likun7981@gmail.com>
2017-10-27 12:19:24 +0800

    injectBabelPlugin support Array<String>, add compose for rewires

--- packages/react-app-rewired/index.js
+++ packages/react-app-rewired/index.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 const babelLoaderMatcher = function(rule) {
   return rule.loader && rule.loader.indexOf(`babel-loader${path.sep}`) != -1;
-}
+};
 
 const getLoader = function(rules, matcher) {
   var loader;
 
   rules.some(rule => {
-    return loader = matcher(rule)
+    return (loader = matcher(rule)
       ? rule
-      : getLoader(rule.use || rule.oneOf || [], matcher);
+      : getLoader(rule.use || rule.oneOf || [], matcher));
   });
 
   return loader;
@@ -18,19 +18,31 @@ const getLoader = function(rules, matcher) {
 
 const getBabelLoader = function(rules) {
   return getLoader(rules, babelLoaderMatcher);
-}
+};
 
 const injectBabelPlugin = function(pluginName, config) {
   const loader = getBabelLoader(config.module.rules);
   if (!loader) {
-    console.log("babel-loader not found");
+    console.log('babel-loader not found');
     return config;
   }
-  
+  const plugins = Array.isArray(pluginName) ? pluginName : [pluginName];
   // Older versions of webpack have `plugins` on `loader.query` instead of `loader.options`.
   const options = loader.options || loader.query;
-  options.plugins = [pluginName].concat(options.plugins || []);
+  options.plugins = plugins.concat(options.plugins || []);
   return config;
+};
+
+const compose = function(...funcs) {
+  if (funcs.length === 0) {
+    return config => config;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (config, env) => a(b(config, env), env));
 };
 
 module.exports = { getLoader, getBabelLoader, injectBabelPlugin };
