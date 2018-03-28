8f8c83b562bb67a6d598964fdaea434905fd8e30
David Schovanec <schovanec@schovi.cz>
2018-03-28 14:59:46 +0200

    Preact: replace mobx-react with mobx-preact (#219)
    based on issue https://github.com/mobxjs/mobx-react/issues/374

--- packages/react-app-rewire-preact/index.js
+++ packages/react-app-rewire-preact/index.js
@@ -2,7 +2,8 @@ function rewirePreact(config, env) {
   config.resolve = {
     "alias": {
       "react": "preact-compat",
-      "react-dom": "preact-compat"
+      "react-dom": "preact-compat",
+      "mobx-react": "mobx-preact"
     }
   }
 
