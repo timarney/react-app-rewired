d89456550e30b73d19aeb9e40e5959031c6873b0
Vladimir Kalmykov <v.kalmykov@infotech.team>
2018-09-10 22:49:13 +0300

    paths-overrides

--- packages/react-app-rewired/scripts/build.js
+++ packages/react-app-rewired/scripts/build.js
@@ -9,5 +9,11 @@ const webpackConfig = require(webpackConfigPath);
 // override config in memory
 require.cache[require.resolve(webpackConfigPath)].exports =
   overrides.webpack(webpackConfig, process.env.NODE_ENV);
+// override paths in memory
+if (paths.pathsOverrides) {
+  const pathsOverridesFn = require(paths.pathsOverrides);
+  require.cache[require.resolve(paths.scriptVersion + '/config/paths')].exports =
+    pathsOverridesFn(paths.originPaths, process.env.NODE_ENV);
+}
 // run original script
 require(paths.scriptVersion + '/scripts/build');
