5e3dea6a1b44f5637cc75e56822643705abdcc2d
David Granado <dgranado@glgroup.com>
2018-10-05 10:44:29 -0500

    Upgrade mobx rewired to work with CRA2

--- packages/react-app-rewire-mobx/index.js
+++ packages/react-app-rewire-mobx/index.js
@@ -1,7 +1,10 @@
 const {injectBabelPlugin} = require('react-app-rewired');
 
 function rewireMobX(config, env) {
-  return injectBabelPlugin('transform-decorators-legacy', config);
+  return injectBabelPlugin([
+    '@babel/plugin-proposal-decorators', {
+        legacy: true,
+    }], config);
 }
 
 module.exports = rewireMobX;
--- packages/react-app-rewired/examples/configurable-overrides/custom-overides/index.js
+++ packages/react-app-rewired/examples/configurable-overrides/custom-overides/index.js
@@ -2,6 +2,6 @@ const { injectBabelPlugin } = require('react-app-rewired');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  config = injectBabelPlugin('babel-plugin-transform-decorators-legacy', config);
+  config = injectBabelPlugin('@babel/plugin-proposal-decorators', config);
   return config;
 }
