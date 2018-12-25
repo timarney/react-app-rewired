d7aae72f8164233379f690010892033479a1e5f4
vdfor <jiakun.li@qq.com>
2018-12-25 18:24:04 +0800

    react-app-rewired support react-scripts more than 2.1.2 version

--- packages/react-app-rewired/scripts/build.js
+++ packages/react-app-rewired/scripts/build.js
@@ -2,12 +2,16 @@ process.env.NODE_ENV = 'production';
 
 const paths = require('./utils/paths');
 const overrides = require('../config-overrides');
-const webpackConfigPath = paths.scriptVersion + "/config/webpack.config.prod";
+const scriptPkg = require(paths.scriptVersion + "/package.json");
+
+const isOldScript = !(scriptPkg && scriptPkg.version >= '2.1.2');
+
+const webpackConfigPath = paths.scriptVersion + isOldScript ? "/config/webpack.config.prod" : "/config/webpack.config";
 
 // load original config
 const webpackConfig = require(webpackConfigPath);
 // override config in memory
 require.cache[require.resolve(webpackConfigPath)].exports =
-  overrides.webpack(webpackConfig, process.env.NODE_ENV);
+  isOldScript ? overrides.webpack(webpackConfig, process.env.NODE_ENV) : (env) => overrides.webpack(webpackConfig(env), env);
 // run original script
 require(paths.scriptVersion + '/scripts/build');
