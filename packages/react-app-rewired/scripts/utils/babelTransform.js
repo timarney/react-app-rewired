f14987a28737163b5c909558e4b83b35ffe33bb7
Danylo Markov <danylo.markov_ges@goeuro.com>
2019-01-09 11:05:02 +0200

    Revert removing babelTransform.js

---
+++ packages/react-app-rewired/scripts/utils/babelTransform.js
@@ -0,0 +1,7 @@
+const babelJest = require('babel-jest');
+
+module.exports = babelJest.createTransformer({
+  presets: [require.resolve('babel-preset-react-app')],
+  plugins: [],
+  babelrc: true
+});
