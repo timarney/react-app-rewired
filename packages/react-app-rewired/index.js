f76c53668da916311266b019c60b48f500b63661
Leif DeJong <leifdejong@gmail.com>
2019-01-18 02:44:46 -0600

    Adding helper deprecation errors (#357)

--- packages/react-app-rewired/index.js
+++ packages/react-app-rewired/index.js
@@ -1,5 +1,24 @@
 const paths = require('./scripts/utils/paths');
 
+function deprecate(helper) {
+  throw new Error(`The "${helper}" helper has been deprecated as of v2.0. You can use customize-cra plugins in replacement - https://github.com/arackaf/customize-cra#available-plugins`);
+}
+
 module.exports = {
+  getLoader: function() {
+    return deprecate('getBabelLoader');
+  },
+  loaderNameMatches: function() {
+    return deprecate('loaderNameMatches');
+  },
+  getBabelLoader: function() {
+    return deprecate('getBabelLoader');
+  },
+  injectBabelPlugin: function() {
+    return deprecate('injectBabelPlugin');
+  },
+  compose: function() {
+    return deprecate('compose');
+  },
   paths
 };
