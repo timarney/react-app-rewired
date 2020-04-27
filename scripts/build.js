067f7616e4e3ca7a24b014d11f72cdcaf1b2cf0d
Ben Wiley <ben.wiley@elementai.com>
2020-04-27 15:13:33 -0400

    Load paths config before webpack config.

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,13 @@ const { scriptVersion } = require('./utils/paths');
 const overrides = require('../config-overrides');
 const scriptPkg = require(`${scriptVersion}/package.json`);
 
+const pathsConfigPath = `${scriptVersion}/config/paths.js`;
+const pathsConfig = require(pathsConfigPath);
+
+// override paths in memory
+require.cache[require.resolve(pathsConfigPath)].exports =
+  overrides.paths(pathsConfig, process.env.NODE_ENV);
+
 // CRA 2.1.2 switched to using a webpack config factory
 // https://github.com/facebook/create-react-app/pull/5722
 // https://github.com/facebook/create-react-app/releases/tag/v2.1.2
@@ -18,13 +25,6 @@ const webpackConfig = require(webpackConfigPath);
 require.cache[require.resolve(webpackConfigPath)].exports = isWebpackFactory
   ? (env) => overrides.webpack(webpackConfig(env), env)
   : overrides.webpack(webpackConfig, process.env.NODE_ENV);
-
-const pathsConfigPath = `${scriptVersion}/config/paths.js`;
-const pathsConfig = require(pathsConfigPath);
-
-// override paths in memory
-require.cache[require.resolve(pathsConfigPath)].exports =
-  overrides.paths(pathsConfig, process.env.NODE_ENV);
 
 // run original script
 require(`${scriptVersion}/scripts/build`);
