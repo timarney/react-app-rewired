2896d809462f423de28bf04927eeac7ee0f0d6bd
bencergazda <benedek.kaldi@srg.hu>
2022-01-06 13:57:41 +0100

    fix: Cannot start `test` script if config-overrides path is set via command line argument

--- scripts/utils/paths.js
+++ scripts/utils/paths.js
@@ -17,9 +17,10 @@ var config_overrides = customPath
   : `${ projectDir }/config-overrides`;
 const co_index = process.argv.indexOf('--config-overrides');
 
+var has_co_argument = false;
 if (co_index > -1 && co_index + 1 <= process.argv.length) {
+  has_co_argument = true;
   config_overrides = path.resolve(process.argv[co_index + 1]);
-  process.argv.splice(co_index, 2);
 }
 
 const scriptVersion = custom_scripts || 'react-scripts';
@@ -33,5 +34,6 @@ const paths = require(modulePath + '/config/paths');
 module.exports = Object.assign({
   scriptVersion: modulePath,
   configOverrides: config_overrides,
-  customScriptsIndex: (custom_scripts ? cs_index : -1)
+  customScriptsIndex: (custom_scripts ? cs_index : -1),
+  configOverridesIndex: (has_co_argument ? co_index : -1),
 }, paths);
