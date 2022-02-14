6372ca2c90549cc7d74fcbff844120d93787d315
bencergazda <bencergazda@users.noreply.github.com>
2022-02-14 21:12:12 +0100

    feat: Mock react-scripts 'config' folder (#593)
    * feat: Add `config` folder with `paths.js`, `webpack.config.js` and `webpackDevServer.config.js`
    
    * fix: Remove duplicated `paths` override from `overrides/webpack.js`
    
    * fix: `paths` overrides also in `config/webpack.config.js` and `config/webpackDevServer.config.js`

---
+++ overrides/jest.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const paths = require('./paths');
+const overrides = require('../config-overrides');
+const rewireJestConfig = require('../scripts/utils/rewireJestConfig');
+const createJestConfigPath = `${paths.scriptVersion}/scripts/utils/createJestConfig`;
+
+// hide overrides in package.json for CRA's original createJestConfig
+const packageJson = require(paths.appPackageJson);
+const jestOverrides = packageJson.jest;
+delete packageJson.jest;
+// load original createJestConfig
+const createJestConfig = require(createJestConfigPath);
+// run original createJestConfig
+const config = createJestConfig(
+	relativePath => path.resolve(paths.appPath, 'node_modules', paths.scriptVersion, relativePath),
+	path.resolve(paths.appSrc, '..'),
+	false
+);
+// restore overrides for rewireJestConfig
+packageJson.jest = jestOverrides;
+// override createJestConfig in memory
+require.cache[require.resolve(createJestConfigPath)].exports =
+	() => overrides.jest(rewireJestConfig(config));
+// Passing the --scripts-version on to the original test script can result
+// in the test script rejecting it as an invalid option. So strip it out of
+// the command line arguments before invoking the test script.
+if (paths.customScriptsIndex > -1) {
+	process.argv.splice(paths.customScriptsIndex, 2);
+}
+
+module.exports = require(createJestConfigPath);
--- scripts/test.js
+++ scripts/test.js
@@ -1,43 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
-const path = require("path");
-let paths = require("./utils/paths");
-const overrides = require('../config-overrides');
-const rewireJestConfig = require("./utils/rewireJestConfig");
-const createJestConfigPath = paths.scriptVersion + "/scripts/utils/createJestConfig";
-
-const pathsConfigPath = `${paths.scriptVersion}/config/paths.js`;
-const pathsConfig = require(pathsConfigPath);
-
-// extend paths with overrides
-paths = Object.assign({}, paths, overrides.paths(pathsConfig, process.env.NODE_ENV));
+const { scriptVersion } = require('./utils/paths');
 
 // override paths in memory
-require.cache[require.resolve(pathsConfigPath)].exports =
-  paths;
+require('../overrides/paths');
 
-// hide overrides in package.json for CRA's original createJestConfig
-const packageJson = require(paths.appPackageJson);
-const jestOverrides = packageJson.jest;
-delete packageJson.jest;
-// load original createJestConfig
-const createJestConfig = require(createJestConfigPath);
-// run original createJestConfig
-const config = createJestConfig(
-  relativePath => path.resolve(paths.appPath, "node_modules", paths.scriptVersion, relativePath),
-  path.resolve(paths.appSrc, ".."),
-  false
-);
-// restore overrides for rewireJestConfig
-packageJson.jest = jestOverrides;
 // override createJestConfig in memory
-require.cache[require.resolve(createJestConfigPath)].exports =
-  () => overrides.jest(rewireJestConfig(config));
-// Passing the --scripts-version on to the original test script can result
-// in the test script rejecting it as an invalid option. So strip it out of
-// the command line arguments before invoking the test script.
-if (paths.customScriptsIndex > -1) {
-  process.argv.splice(paths.customScriptsIndex, 2);
-}
+require('../overrides/jest');
+
 // run original script
-require(paths.scriptVersion + '/scripts/test');
+require(`${scriptVersion}/scripts/test`);
